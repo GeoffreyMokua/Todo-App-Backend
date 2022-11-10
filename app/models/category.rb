15f275665d88bd9adf7ef45bf371fa6580ee15bd
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-10 10:33:50 +0300

    added category model

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Base
+    has_many :todos, dependent: :destroy
+    has_many :users, through: :todos
+
+    validates :category, presence: { message: " %{attribute} can't be blank." }
+
+end
