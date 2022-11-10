b827b9fe20a513eb6aff1cba857d24532e2c9260
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-10 09:14:33 +0300

    added the categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_061134) do
+ActiveRecord::Schema.define(version: 2022_11_10_061325) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category"
+    t.datetime "deleted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
