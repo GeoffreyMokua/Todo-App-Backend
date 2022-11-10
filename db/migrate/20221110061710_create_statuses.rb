2d5f9743a93861577654cefb62056f5bf2b6b4b2
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-10 09:17:53 +0300

    added status table

---
+++ db/migrate/20221110061710_create_statuses.rb
@@ -0,0 +1,4 @@
+class CreateStatuses < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_061325) do
+ActiveRecord::Schema.define(version: 2022_11_10_061710) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
